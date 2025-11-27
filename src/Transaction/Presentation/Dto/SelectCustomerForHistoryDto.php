7cff2e7ce76f2f8292caa5ebe98e281b2b30ca71
Emil Sosnowski <emil.sosnowski@pragmago.tech>
2025-11-27 12:42:30 +0100

    Nicer validation messages

--- src/Transaction/Presentation/Dto/SelectCustomerForHistoryDto.php
+++ src/Transaction/Presentation/Dto/SelectCustomerForHistoryDto.php
@@ -5,11 +5,22 @@ declare(strict_types=1);
 namespace App\Transaction\Presentation\Dto;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 final class SelectCustomerForHistoryDto
 {
-    #[Assert\NotBlank(message: 'Please select a customer')]
     public ?string $customerId = null;
 
     public ?string $bankAccountId = null;
+
+    #[Assert\Callback]
+    public function validatePasswordsMatch(ExecutionContextInterface $context): void
+    {
+        if (null === $this->customerId) {
+            $context->buildViolation('transaction.validation.please_select_customer')
+                ->atPath('customerId')
+                ->setTranslationDomain('transaction')
+                ->addViolation();
+        }
+    }
 }
